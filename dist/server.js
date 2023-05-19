9b9afc3f6793da0b893ac0994293ec878a1d1b10
beauspot <beauspot@outlook.com>
2023-05-19 09:28:16 +0100

    setting up the server or app entery point in this rest api. there are some errors to be fixed.

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,70 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv = __importStar(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
+const connect_mongodb_session_1 = __importDefault(require("connect-mongodb-session"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express_session_1 = __importDefault(require("express-session"));
+// import xssClean from "xss-clean";
+dotenv.config({ path: __dirname + "/.env" });
+const port = process.env.PORT || 2000;
+const app = (0, express_1.default)();
+const MongoDBStore = (0, connect_mongodb_session_1.default)(express_session_1.default);
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URL,
+    collection: "sessions",
+    ttl: 60 * 60,
+});
+app.use((0, cors_1.default)());
+app.use((0, helmet_1.default)());
+app.use((0, express_mongo_sanitize_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use((0, morgan_1.default)("dev"));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+if (process.env.NODE_ENV !== "development")
+    app.use((0, morgan_1.default)("dev"));
+app.use((0, express_session_1.default)({
+    resave: false,
+    secret: process.env.SESSION_SECRET_KEY,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+        sameSite: "strict",
+        secure: false,
+        maxAge: 1000 * 60 * 60, // cookie would expire in 1 hour
+    },
+}));
+app.listen(port, () => {
+    console.log(`Server listing at http://localhost:${port}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,55 @@
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import lodash from "lodash";
+import mongoSanitize from "express-mongo-sanitize";
+import { StatusCodes } from "http-status-codes";
+import MongodbSession from "connect-mongodb-session";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import logger from "morgan";
+// import xssClean from "xss-clean";
+
+dotenv.config({path: __dirname+"/.env"});
+
+const port = process.env.PORT || 2000;
+
+const app = express();
+const MongoDBStore = MongodbSession(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URL as string,
+    collection: "sessions",
+    ttl: 60 * 60,
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+if (process.env.NODE_ENV !== "development") app.use(morgan("dev"));
+app.use(
+  session({
+    resave: false,
+    secret: process.env.SESSION_SECRET_KEY as string,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      sameSite: "strict",
+      secure: false, // use true if using https
+      maxAge: 1000 * 60 * 60, // cookie would expire in 1 hour
+    },
+  })
+);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server listing at http://localhost:${port}`);
+});
